6422760a5e7a81fdeb3bdf11fafed0c86339aa8f
Sureshkumar S <>
2024-01-28 12:06:03 +0530

    cypress added

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,6 +2,17 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   projectId: '7ma4iz',
+  "reporter": "cypress-multi-reporters",
+  "reporterOptions": {
+      "reporterEnabled": "mochawesome",
+      "mochawesomeReporterOptions": {
+          "reportDir": "cypress/reports/mocha",
+          "quite": true,
+          "overwrite": false,
+          "html": true,
+          "json": true
+      }
+  },
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
